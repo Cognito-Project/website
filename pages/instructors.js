fbfa59cf5d9139174e32a9322fb801cc21486c89
Joshua Martinez <joshkmartinez@gmail.com>
2019-12-28 23:57:52 -0800

    remove undeed pages

--- pages/instructors.js
+++
@@ -1,8 +0,0 @@
-const instructors = () => (
-  <meta
-    http-equiv="Refresh"
-    content="0; url=https://forms.gle/qDmQ1b4vRBTjZqbS7"
-  />
-);
-
-export default instructors;
--- pages/register.js
+++
@@ -1,5 +0,0 @@
-const register = () => (
-  <meta http-equiv="Refresh" content="0; url=https://discord.gg/aeMPpdv" />
-);
-
-export default register;
