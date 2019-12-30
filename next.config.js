82581be4285fb0c915893360b02ce37158f7897d
Joshua Martinez <joshkmartinez@gmail.com>
2019-12-29 21:50:49 -0800

    serverless

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 const withImages = require("next-images");
-const withCSS = require('@zeit/next-css')
-module.exports = withCSS(withImages({
-  //target: "serverless",
-  /*exportPathMap: function() {
+const withCSS = require("@zeit/next-css");
+module.exports = withCSS(
+  withImages({
+    target: "serverless"
+    /*exportPathMap: function() {
     return {
       "/": { page: "/" },
       "/register": { page: "/register" }
     };
   }*/
-}));
+  })
+);
